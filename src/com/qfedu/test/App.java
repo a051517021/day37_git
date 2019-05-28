4b6033778eebde69f989000e703aef9279754c6d
a051517021 <51064509+a051517021@users.noreply.github.com>
2019-05-28 14:23:16 +0800

    qq

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,7 +3,7 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("asdfg");
-        System.out.println("asdfg");
+//         System.out.println("asdfg");
         int a = 10;
 
     }
