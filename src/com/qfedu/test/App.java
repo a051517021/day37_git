a8c5c55c101629e58ac7bda9703ff37731d5e30c
a051517021 <3041323114@qq.com>
2019-05-28 14:04:57 +0800

    change info

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,8 +3,8 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("asdfg");
+        System.out.println("asdfg");
+        System.out.println("asdfg");
 
-        System.out.println("asdfg");
-        System.out.println("asdfg");
     }
 }
