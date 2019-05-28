8033698d2cf1990f4ebd1fcd6572c5c7e86e8a5a
a051517021 <3041323114@qq.com>
2019-05-28 14:06:06 +0800

    11

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("asdfg");
         System.out.println("asdfg");
-        System.out.println("asdfg");
+        int a = 10;
 
     }
 }
